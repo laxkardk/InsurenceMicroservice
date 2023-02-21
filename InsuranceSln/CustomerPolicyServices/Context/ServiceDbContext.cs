c27db14e1abecac70b0e2997658958d48794b04c
imran4fun <39696108+imran4fun@users.noreply.github.com>
2023-02-21 15:08:28 +0530

    Update ServiceDbContext.cs
    removed commented code

--- InsuranceSln/CustomerPolicyServices/Context/ServiceDbContext.cs
+++ InsuranceSln/CustomerPolicyServices/Context/ServiceDbContext.cs
@@ -17,15 +17,6 @@ namespace CustomerPolicyServices.Context
             return await base.SaveChangesAsync();
         }
 
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    if (!optionsBuilder.IsConfigured)
-        //    {
-        //        //optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False");
-        //        optionsBuilder.UseSqlServer("Data Source=(localdb)\\ProjectModels;Initial Catalog=CustomerPolicyDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-
-        //    }
-        //    base.OnConfiguring(optionsBuilder);
-        //}
-    }
+       
+       }
 }
